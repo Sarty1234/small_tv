b6de7de94cfa2e642a66d5c9502989fed6981da4
Sarty1234 <74873961+Sarty1234@users.noreply.github.com>
2026-05-26 10:23:12 +0300

    added hotkey listening with sharphook

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,10 +5,11 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
+using SharpHook.Data;
+using SharpHook;
+using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace smallTV
 {
@@ -17,6 +18,9 @@ namespace smallTV
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly List<Key> PressedKeys = new List<Key>();
+        private static SimpleGlobalHook _hook;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -25,12 +29,87 @@ namespace smallTV
             UpdateKeyLabels();
         }
 
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _hook = new SimpleGlobalHook();
+
+            _hook.KeyPressed += OnKeyPressed;
+            _hook.KeyReleased += OnKeyReleased;
+
+
+            await Task.Run(() => _hook.Run());
+        }
+
+
+
+        private void OnKeyPressed(object sender, KeyboardHookEventArgs e)
+        {
+            lock (PressedKeys) {
+                Key pressedKey = KeyInterop.KeyFromVirtualKey((int)e.Data.KeyCode);
+                PressedKeys.Add(pressedKey);
+
+
+
+
+                // Check in keys alligning
+                if (PressedKeys != null)
+                {
+                    if (MenuKeys != null && MenuKeys.Contains(HotkeyResetFinishKey) 
+                        && PressedKeys.SequenceEqual(MenuKeys[1..]))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            MenuFunction();
+                        });
+                    }
+
+
+                    if (TVKeys != null && TVKeys.Contains(HotkeyResetFinishKey)
+                        && PressedKeys.SequenceEqual(TVKeys[1..]))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            TVFunction();
+                        });
+                    }
+
+
+                    if (PanicKeys != null && PanicKeys.Contains(HotkeyResetFinishKey)
+                        && PressedKeys.SequenceEqual(PanicKeys[1..]))
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            PanicFunction();
+                        });
+                    }
+                }
+            }
+        }
+
+
+        private void OnKeyReleased(object sender, KeyboardHookEventArgs e)
+        {
+            lock (PressedKeys)
+            {
+                Key releasedKey = KeyInterop.KeyFromVirtualKey((int)e.Data.KeyCode);
+                PressedKeys.Remove(releasedKey);
+            }
+        }
+
+
+
+
+
+
+
+
 
         // Params
         Key HotkeyResetFinishKey => Config.Instance.HotkeyResetFinishKey;
         List<Key> MenuKeys = Config.Instance.MenuKeys;
         List<Key> TVKeys = Config.Instance.TVKeys;
         List<Key> PanicKeys = Config.Instance.PanicKeys;
+
 
 
         private void ResetMenuKeysButton_Click(object sender, RoutedEventArgs e)
@@ -178,6 +257,34 @@ namespace smallTV
 
 
         private void SelectWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+        int b = 0;
+        public void MenuFunction()
+        {
+            b += 1;
+            MenuKeysLabel.Content = b;
+
+            //MessageBox.Show("hi");
+        }
+
+
+        public void TVFunction()
+        {
+
+        }
+
+
+        public void PanicFunction()
         {
 
         }
